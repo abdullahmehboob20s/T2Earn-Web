d54b77d527618dbc4c6e2b20325764192914e651
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2021-11-01 14:35:23 +0500

    s

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,29 +57,47 @@ $(".our-services-cards").slick({
   ],
 });
 
-const toHelpAccordion = document.querySelectorAll(".to-help-accordion");
+$(".customers-right-wrapper").slick({
+  dots: false,
+  infinite: false,
+  speed: 700,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow: ` <button class="customers-card-btn">
+  <img  src="./assets/images/chevron-right.png" alt="" />
+</button>`,
+  prevArrow: ` <button class="customers-card-btn">
+  <img  src="./assets/images/chevron-left.png" alt="" />
+</button>`,
+});
+
+// for accordions ===================================
+
 const toHelpAccordionBtn = document.querySelectorAll(".to-help-accordion-btn");
+const footerFaqBtn = document.querySelectorAll(".footer-faq-btn");
 
-for (let i = 0; i < toHelpAccordionBtn.length; i++) {
-  toHelpAccordionBtn[i].addEventListener("click", () => {
-    let accordionBody = toHelpAccordionBtn[i].parentElement.querySelector(
-      ".to-help-accordion-body"
-    );
-    let accordionIcon = toHelpAccordionBtn[i].parentElement.querySelector(
-      ".to-help-accordion-icon"
-    );
+let accordionFunction = (btn, body, showDash) => {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      let parent = btn[i].parentElement;
+      let accordionBody = parent.querySelector(body);
 
-    if (accordionBody.clientHeight == 0) {
-      accordionBody.style.height = accordionBody.scrollHeight + "px";
-      accordionIcon.src = "./assets/images/dash-icon.png";
-    } else {
-      accordionIcon.src = "./assets/images/plus-icon.png";
-      accordionBody.style.height = 0;
-    }
-  });
-}
+      if (accordionBody.clientHeight == 0) {
+        accordionBody.style.height = accordionBody.scrollHeight + "px";
+        parent.classList.add("accordion-open");
+      } else {
+        accordionBody.style.height = 0;
+        parent.classList.remove("accordion-open");
+      }
+    });
+  }
+};
 
-// for sidebar in responsive
+accordionFunction(toHelpAccordionBtn, ".to-help-accordion-body", true);
+accordionFunction(footerFaqBtn, ".footer-faq-body", false);
+
+// for sidebar in responsive ================================
 const navbarItems = document.querySelector(".navbar-items");
 const hamburger = document.querySelector(".hamburger");
 const navbarCrossIcon = document.querySelector(".navbar-cross-icon");
